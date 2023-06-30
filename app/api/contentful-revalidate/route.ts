a6f49b487c3401b198a79565e5f7db8d0171eac0
robsoncoelho <robson.amc@gmail.com>
2023-06-30 19:33:25 +0100

    fix: request method

--- app/api/contentful-revalidate/route.ts
+++ app/api/contentful-revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 import { headers } from 'next/headers'
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const headersList = headers()
   // should be secret, custom header coming in from Contentful
   const inboundRevalToken = headersList.get('x-vercel-reval-key')
